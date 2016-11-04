a920f91f2c9b445bfabd622e9c4d15ff5dc4cfa0
John Villavicencio <jjvillavicencio1@gmail.com>
2016-11-03 20:34:19 -0500

    Compartir redes Sociales

--- www/js/controllers/infCtrl.js
+++ www/js/controllers/infCtrl.js
@@ -1,4 +1,4 @@
-starter.controller('infCtrl', function ($scope, $firebaseArray, $firebaseAuth, $location) {
+starter.controller('infCtrl', function ($scope, $firebaseArray, $firebaseAuth, $location, $cordovaSocialSharing) {
 
     //---Recupero array de promociones
     var misPromociones = firebase.database().ref().child('promociones');
@@ -12,5 +12,32 @@ starter.controller('infCtrl', function ($scope, $firebaseArray, $firebaseAuth, $
     $scope.toRoom = function () {
         $location.url("/roomService");
     };
+    $scope.compTwitter = function(message, image){
+        $cordovaSocialSharing
+        .shareViaTwitter(message, image)
+        .then(function(result) {
+        // Success!
+        }, function(err) {
+        // An error occurred. Show a message to the user
+        });
+    };
 
+    $scope.compWhats = function(message, image){
+        $cordovaSocialSharing
+        .shareViaWhatsApp(message, image)
+        .then(function(result) {
+        // Success!
+        }, function(err) {
+        // An error occurred. Show a message to the user
+        });
+    };
+    $scope.compFb = function(message, image){
+        $cordovaSocialSharing
+        .shareViaFacebook(message, image)
+        .then(function(result) {
+        // Success!
+        }, function(err) {
+        // An error occurred. Show a message to the user
+        });
+    };
 });
